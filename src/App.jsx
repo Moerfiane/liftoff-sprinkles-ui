64c4f76d163465c172a27a3e7600e326bfefa805
Madeline <moon.madeline@gmail.com>
2023-12-12 21:18:25 -0600

    Import Bootstrap styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import CreateAccountForm from './components/CreateAccountForm'
+import HorizontalExample from './components/HorizontalFormExample'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [count, setCount] = useState(0)
