0fa64a0aef20424f98d34b3ab2390fd3066f0516
Madeline <moon.madeline@gmail.com>
2023-12-15 12:39:08 -0600

    Outline further TODOs

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,15 @@ import Container  from 'react-bootstrap/Container';
 
 //Done: Build CourseCard component
 //TODO: Build Menu component
+//TODO: Build Sign up component
+//TODO: Build Login component
+//TODO: Build Hero component
+//TODO: Write about content
 //TODO: Build Account Management screen
 //TODO: Build CoursePage screen
 //TODO: Build CourseCompletion screen
 //TODO: Build Search screen
+//TODO: Build AddCourse screen
 //TODO: Figure out how bootstrap is calculating width by comparing HorizontalExample to CreateAccountForm
 //TODO: Set useState variables to refresh page based on menu items clicked
 
