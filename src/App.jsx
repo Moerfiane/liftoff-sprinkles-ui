6d76e49dd4b3da7ddb3526659689127e3fc91def
Madeline <moon.madeline@gmail.com>
2023-12-30 11:23:09 -0600

    Update TODOs

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,20 @@ import Register from './components/Register';
 import CreateCourse from './components/CreateCourseForm';
 import ViewCourses from './components/ViewAllCourses';
 
+//Done: Build CourseCard component
+//Done: Build Menu component
+//Done: Build Sign up component
+//Done: Build Login component
+//TODO: Build Hero component
+//TODO: Write about content
+//TODO: Build Account Management screen
+//TODO: Build CoursePage screen
+//TODO: Build CourseCompletion screen
+//TODO: Build Search screen
+//In progress: Build AddCourse screen
+//Done: Set routes using react-router Set useState variables to refresh page based on menu items clicked
+
+
 
 function App() {
   return (
