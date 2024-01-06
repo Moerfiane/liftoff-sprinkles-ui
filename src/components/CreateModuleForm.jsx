de0c49f5738f3a167922e4da0016b9d187037d8d
Madeline <moon.madeline@gmail.com>
2024-01-06 16:41:29 -0600

    Moved ModuleForm to bottom and changed name fields

--- src/components/CreateModuleForm.jsx
+++ src/components/CreateModuleForm.jsx
@@ -5,69 +5,21 @@ import { sendData } from '../utilities/sendData';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import axios from "axios";
 
-const ModuleForm = ({handleChange, handleClick}) => {
-    return (
-        <>
-            <h2 className="fw-bold mb-1 text-uppercase">Add a Module</h2>
-            <h3 className="mb-5">Course Title</h3>
-            <Form.Group className="mb-1 mt-4" controlId="formCourseTitle">
-                <Form.Label className="text-center">
-                    Module Title
-                </Form.Label>
-                <Form.Control type="text" name="name" placeholder="Enter a course title" />
-            </Form.Group>
-            <Form.Group className="mb-1 mt-4" controlId="formModuleDescription">
-                <Form.Label className="text-center">
-                    Module Description
-                </Form.Label>
-                <Form.Control as="textarea" name="description" placeholder="Enter a description" onChange={handleChange}></Form.Control>
-            </Form.Group>
-            <Form.Group className="mb-1 mt-4" controlId="formModuleTools">
-                <Form.Label className="text-center">
-                    Module Tools
-                </Form.Label>
-                <Form.Control as="textarea" name="tools" placeholder="Enter a list of tools" onChange={handleChange}></Form.Control>
-            </Form.Group>
-            <Form.Group className="mb-1 mt-4" controlId="formModuleIngredients">
-                <Form.Label className="text-center">
-                    Module Ingredients
-                </Form.Label>
-                <Form.Control as="textarea" name="ingredients" placeholder="Enter a list of Ingredients" onChange={handleChange}></Form.Control>
-            </Form.Group>
-            <Form.Group className="mb-1 mt-4" controlId="formModuleNotes">
-                <Form.Label className="text-center">
-                    Module Notes
-                </Form.Label>
-                <Form.Control as="textarea" name="notes" placeholder="Enter any notes relevant to the recipe" onChange={handleChange}></Form.Control>
-            </Form.Group>
-            <Form.Group className="mb-1 mt-4" controlId="formModuleSteps">
-                <Form.Label className="text-center">
-                    Module Steps
-                </Form.Label>
-                <Form.Control as="textarea" name="steps" placeholder="Enter the steps of the recipe" onChange={handleChange}></Form.Control>
-            </Form.Group>
-            <div className="d-grid mt-5">
-                <Button variant="primary" type="submit" name="addModule" onClick={handleClick}>
-                    Add Module
-                </Button>
-            </div>
-        </>
-    )
-}
-
 //TODO: Add category
 export default function CreateModule() {
     const location = useLocation();
     const response = location.state;
-    const [moduleData, setModuleData] = useState({
+    const initialModuleData = {
         courseId: response,
-        name:'',
-        description:'',
-        tools:'',
-        ingredients:'',
-        notes:'',
-        steps:'',
-    });
+        name: '',
+        description: '',
+        tools: '',
+        ingredients: '',
+        notes: '',
+        steps: '',
+    };
+
+    const [moduleData, setModuleData] = useState(initialModuleData);
 
     const navigate = useNavigate();
 
@@ -84,6 +36,7 @@ export default function CreateModule() {
 
         if (e.target.name == "addModule") {
             navigate("/courses/modules/create");
+            setModuleData(initialModuleData);
         } else {
             navigate("/courses");
         }
@@ -92,8 +45,6 @@ export default function CreateModule() {
 
         sendData('/courses/modules/create', 'POST', {'Content-Type': 'application/json'}, moduleData);
     };
-
-
         
   return (
     <>
@@ -104,7 +55,7 @@ export default function CreateModule() {
                     <Card>
                         <Card.Body>
                         <div className="mb-3 mt-4">
-                            <ModuleForm handleChange={handleChange} handleClick={handleClick} />
+                            <ModuleForm handleChange={handleChange} handleClick={handleClick} moduleData={moduleData} />
                         </div>
                         </Card.Body>
                     </Card>
@@ -113,4 +64,59 @@ export default function CreateModule() {
         </Container>
     </>
   );
+}
+
+const ModuleForm = ({handleChange, handleClick, moduleData}) => {
+    return (
+        <>
+            <h2 className="fw-bold mb-1 text-uppercase">Add a Module</h2>
+            <h3 className="mb-5">Course Title</h3>
+            <Form.Group className="mb-1 mt-4" controlId="formCourseTitle">
+                <Form.Label className="text-center">
+                    Module Title
+                </Form.Label>
+                <Form.Control type="text" name="name" placeholder="Enter a course title" value={moduleData.name} onChange={handleChange} />
+            </Form.Group>
+            <Form.Group className="mb-1 mt-4" controlId="formModuleDescription">
+                <Form.Label className="text-center">
+                    Module Description
+                </Form.Label>
+                <Form.Control as="textarea" name="description" placeholder="Enter a description" value={moduleData.description} onChange={handleChange}></Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-1 mt-4" controlId="formModuleTools">
+                <Form.Label className="text-center">
+                    Module Tools
+                </Form.Label>
+                <Form.Control as="textarea" name="tools" placeholder="Enter a list of tools" value={moduleData.tools} onChange={handleChange}></Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-1 mt-4" controlId="formModuleIngredients">
+                <Form.Label className="text-center">
+                    Module Ingredients
+                </Form.Label>
+                <Form.Control as="textarea" name="ingredients" placeholder="Enter a list of Ingredients" value={moduleData.ingredients} onChange={handleChange}></Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-1 mt-4" controlId="formModuleNotes">
+                <Form.Label className="text-center">
+                    Module Notes
+                </Form.Label>
+                <Form.Control as="textarea" name="notes" placeholder="Enter any notes relevant to the recipe" value={moduleData.notes} onChange={handleChange}></Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-1 mt-4" controlId="formModuleSteps">
+                <Form.Label className="text-center">
+                    Module Steps
+                </Form.Label>
+                <Form.Control as="textarea" name="steps" placeholder="Enter the steps of the recipe" value={moduleData.steps} onChange={handleChange}></Form.Control>
+            </Form.Group>
+            <Row className="mt-5">
+                <Button variant="secondary" type="submit" name="addModule" onClick={handleClick}>
+                    Add Another Module
+                </Button>
+            </Row>
+            <Row className="mt-3">
+                <Button variant="primary" type="submit" name="done" onClick={handleClick}>
+                    Done
+                </Button>
+            </Row>
+        </>
+    )
 }
