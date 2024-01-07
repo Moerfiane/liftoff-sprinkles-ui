5533ebe7789a1834707c7a7d4e586871e540dfc6
Madeline <moon.madeline@gmail.com>
2024-01-06 18:40:19 -0600

    Retrieve course data with debugging

--- src/components/ViewAllCourses.jsx
+++ src/components/ViewAllCourses.jsx
@@ -2,9 +2,35 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Navigation from './Navbar';
 import CourseCard from './CourseCard';
+// import getData from '../utilities/getData';
 
 //TODO: Make container flex
 //TODO: Import data from backend
+
+const getData = async () => {
+  try {
+      const response = await fetch(`http://localhost:8080/courses`, {
+          method: 'GET', 
+          headers: {
+            'Content-Type': 'application/json',
+          }, 
+      });
+
+      if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json(); // Assuming response is JSON, adjust accordingly
+
+      return data;
+  } catch (error) {
+      console.error('Error:', error);
+      throw error; // Re-throw the error to propagate it up the call stack
+  }
+};
+
+console.log(await getData());
+
 
 const CourseList = ({ data }) => (
   <>
