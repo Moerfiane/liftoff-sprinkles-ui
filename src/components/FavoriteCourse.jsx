cf014e88feab4620f2dc299b64cf094207bdaf22
Madeline <moon.madeline@gmail.com>
2024-01-18 18:13:14 -0600

    update styling to match/differentiate from view all

--- src/components/FavoriteCourse.jsx
+++ src/components/FavoriteCourse.jsx
@@ -79,15 +79,16 @@ function CourseCard({id, name, description }) {
  
   return (
     <Card style={{ width: '18rem' }} key={id} className="mb-3 mt-3">
-      <Card.Img variant="top" src="/assets/egg.jpg" />
       {/* Photo by <a href="https://unsplash.com/@mustafabashari?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Mustafa Bashari</a> on <a href="https://unsplash.com/photos/white-egg-lot-on-brown-wooden-table-rKUK4EB8F7s?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a> */}
       <Card.Body className="d-flex flex-column h-100">
         <Card.Title>{name}</Card.Title>
         <Card.Text>
           {description}
         </Card.Text>
-        {isLoggedIn ? <Button variant="secondary" onClick={() => handleClick("enroll")}>Enroll</Button> : <Button variant="secondary" onClick={() => handleClick("login")}>Login to enroll</Button>}
-        <Button className="mt-auto" variant="primary" onClick={() => handleClick("details")}>Course details</Button>
+        <div className="d-flex flex-column mt-4 gap-3">
+          {isLoggedIn ? <Button variant="success" onClick={() => handleClick("enroll")}>Enroll</Button> : <Button variant="secondary" onClick={() => handleClick("login")}>Login to enroll</Button>}
+          <Button className="mt-auto" variant="secondary" onClick={() => handleClick("details")}>Course details</Button>
+        </div>
       </Card.Body>
     </Card>
   );
